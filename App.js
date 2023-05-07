2b8a6642ad317f1c15ecbacfbfaedee0ffdffe11
gajojr <andrijagajicbusiness@gmail.com>
2023-05-07 09:33:51 +0200

    chore: add mock data and list it

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const mockData = [
+	{
+		id: 1,
+		name: 'Buy groceries',
+	},
+	{
+		id: 2,
+		name: 'Finish react native course',
+	},
+	{
+		id: 3,
+		name: 'Read a book',
+	},
+];
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.heading}>Todo App</Text>
+			<FlatList
+				data={mockData}
+				renderItem={({ item }) => (
+					<Text style={styles.itemListed}>{item.name}</Text>
+				)}
+				keyExtractor={(item) => item.id}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	heading: {
+		fontSize: 24,
+		fontWeight: 'bold',
+	},
+	itemListed: {
+		fontSize: 18,
+	},
 });
